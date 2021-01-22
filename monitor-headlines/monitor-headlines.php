1fbf2d126a1b7d5ce74cc68ab7b78d362ff03af5
masikotimo <masikotimo@gmail.com>
2021-01-22 14:27:58 +0300

    created the baseline for the plugin so that it can be easily seen

---
+++ monitor-headlines/monitor-headlines.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package monitor-headlines
+ */
+/*
+Plugin Name: Monitor-Headlines
+Plugin URI: https://masiko.com/
+Description: Used by millions, to monitor headlines from the daily monitor newspaper and depict it 
+Version: 0.0.1
+Author: Timothy Masiko
+Author URI: https://masikotimo.github.io/portfolio/
+License: GPLv2 or later
+
+*/
